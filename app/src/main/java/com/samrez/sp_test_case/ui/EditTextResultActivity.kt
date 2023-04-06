7634c00f89dc272ac938ba194f34a588da9958b3
antareza <oneakun08@gmail.com>
2023-04-07 05:40:03 +0700

    Init UI for Edit Text Put Get key for Text Edit

--- app/src/main/java/com/samrez/sp_test_case/ui/EditTextResultActivity.kt
+++ app/src/main/java/com/samrez/sp_test_case/ui/EditTextResultActivity.kt
@@ -1,12 +1,37 @@
 package com.samrez.sp_test_case.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.samrez.sp_test_case.R
+import com.samrez.sp_test_case.databinding.ActivityEditTextResultBinding
 
 class EditTextResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditTextResultBinding
+    private lateinit var textResult: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_text_result)
+        binding = ActivityEditTextResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textResult = intent.getStringExtra("textResult").toString()
+
+        initUI()
+        initListener()
+    }
+
+    private fun initUI() {
+        binding.etEditResult.setText(textResult)
+    }
+
+    private fun initListener() {
+        binding.btnEditResult.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("textResult", binding.etEditResult.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
